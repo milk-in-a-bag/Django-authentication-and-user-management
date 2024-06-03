8528e2be39bd05f9a80e0e2e96b1549ffd086006
bryanmayodi <bryanmayodi@gmail.com>
2024-06-03 13:35:08 +0300

    banning users

--- website/main/views.py
+++ website/main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import RegisterForm, PostForm
 from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User, Group
 from .models import Post
 
 # Create your views here.
@@ -12,10 +13,26 @@ def home(request):
 
     if request.method == 'POST':
         post_id = request.POST.get('post-id')
-        post = Post.objects.filter(id=post_id).first()
+        user_id = request.POST.get('user-id')
 
-        if post and (post.author == request.user or request.user.has_perm("main.delete_post")):
-            post.delete()
+        if post_id:
+            post = Post.objects.filter(id=post_id).first()
+            if post and (post.author == request.user or request.user.has_perm("main.delete_post")):
+                post.delete()
+        elif user_id:
+            user = User.objects.filter(id=user_id).first()
+            if user and request.user.is_staff:
+                try:
+                    group = Group.objects.get(name='default')
+                    group.user_set.remove(user)
+                except:
+                    pass
+
+                try:
+                    group = Group.objects.get(name='mod')
+                    group.user_set.remove(user)
+                except:
+                    pass
 
     return render(request, 'main/home.html', {'posts': posts})
 
